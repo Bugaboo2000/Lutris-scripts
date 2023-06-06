2cee2bf675fc139b2c36f8639cc665a8e1ffb8bc
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-05 22:07:45 -0300

    Working on fix for Carmageddon 1

---
+++ Carmageddon 1/fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_single.conf
+wget https://github.com/Bugaboo2000/Lutris-scripts/releases/download/Layer/nglide21.zip
+unzip nglide21.zip -d $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="libogg-0,liboggvorbis-0,libvorbisfile-3,glide,glide2x,glide3x,msvcp90,msvcr90,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/nglide_config.exe
+
