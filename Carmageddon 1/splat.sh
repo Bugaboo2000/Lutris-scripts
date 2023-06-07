c414eea7496a35baabf0e2ff426fb2db8634448a
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-07 07:36:39 -0300

    Updated dll override

--- Carmageddon 1/splat.sh
+++ Carmageddon 1/splat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="libogg-0,liboggvorbis-0,libvorbisfile-3,glide,glide2x,glide3x,msvcp90,msvcr90,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_single.conf -noconsole -c exit
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,SDL,SDL_net,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_single.conf -noconsole -c exit
