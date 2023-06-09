26ae1c7358f63ade69d482bdd13ba82ee82ecf9a
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-09 07:11:38 -0300

    config fixes

--- Carmageddon 1/carmahost.sh
+++ Carmageddon 1/carmahost.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_server.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_server.conf
-WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="libogg-0,liboggvorbis-0,libvorbisfile-3,glide,glide2x,glide3x,msvcp90,msvcr90,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe  	-conf ..\dosboxCARMA.conf -conf ..\dosboxCARMA_server.conf -noconsole -c exit
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,SDL,SDL_net,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARMA.conf -conf dosboxCARMA_server.conf -noconsole -c exit
