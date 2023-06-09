2eb5b6805e83d940e7e0ec3c96c239447a603b27
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-09 07:08:22 -0300

    Changed dll overrides and added multiplayer

---
+++ Carmageddon 1/carmahost.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_server.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_server.conf
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="libogg-0,liboggvorbis-0,libvorbisfile-3,glide,glide2x,glide3x,msvcp90,msvcr90,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe  	-conf ..\dosboxCARMA.conf -conf ..\dosboxCARMA_server.conf -noconsole -c exit
