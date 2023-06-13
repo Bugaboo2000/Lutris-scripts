1a8179af2dcd304b7e18068e4c4adda1c70b4ef3
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-13 16:04:43 -0300

    Added splatpack configs

--- Carmageddon 1/fix.sh
+++ Carmageddon 1/fix.sh
@@ -16,6 +16,16 @@ wget https://github.com/Bugaboo2000/Lutris-scripts/releases/download/Layer/nglid
 unzip nglide21.zip -d $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ 
 }
 
+splat() {
+
+sed -i 's/output=\(.*\)/output=opengl/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf
+sed -i "s/fullresolution=.*$/fullresolution=$(xrandr | grep '*' | awk '{print $1}')/" config_file $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf
+sed -i 's/memsize=\(.*\)/memsize=512/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
+sed -i 's/glide=\(.*\)/glide=true/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf
+
+
+}
+
 fssetup() {
 WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/nglide_config.exe
 }
@@ -23,5 +33,7 @@ WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDE
 links
 wait
 configs
+wait
+splat
 wait
 fssetup
