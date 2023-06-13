83b68e2232575c291b161b47363ec7cab286fdf1
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-12 22:58:48 -0300

    Added nglide config setup

---
+++ Carmageddon 1/nglide_config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/nglide_config.exe
