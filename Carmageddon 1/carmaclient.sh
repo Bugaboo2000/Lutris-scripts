9fc4ff1e7fe704bd99d9a72bbf186d43a781deb3
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-13 15:30:03 -0300

    fixed shell conflicts with wineprefix

--- Carmageddon 1/carmaclient.sh
+++ Carmageddon 1/carmaclient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_client.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_client.conf
-WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARMA.conf -conf dosboxCARMA_client.conf -noconsole -c exit 	
+cd cd $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ && WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,SDL,SDL_net,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARMA.conf -conf dosboxCARMA_client.conf -noconsole -c exit 	
--- Carmageddon 1/splat.sh
+++ Carmageddon 1/splat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_single.conf -noconsole -c exit
+cd $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ && WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_single.conf -noconsole -c exit
