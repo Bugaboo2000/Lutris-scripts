1596416c4e13b8fcbe6d73b0839e3b782dd81162
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-18 20:38:55 -0300

    Added Readme and updated scripts

--- Carmageddon 1/fix.sh
+++ Carmageddon 1/fix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 links() {
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf $GAMEDIR/DOSBOX/dosboxCARMA.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf $GAMEIDR/DOSBOX/dosboxCARMA_single.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf $GAMEDIR/DOSBOX/dosboxCARSPLAT.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT_single.conf $GAMEDIR/DOSBOX/dosboxCARSPLAT_single.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_single.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARSPLAT.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT_single.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARSPLAT_single.conf
 }
 
 configs() {
@@ -27,7 +27,7 @@ sed -i 's/glide=\(.*\)/glide=true/' $HOME/.local/share/Steam/steamapps/common/Ca
 }
 
 fssetup() {
-WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $GAMEDIR/DOSBOX/nglide_config.exe
+WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/nglide_config.exe
 }
 
 links
--- Carmageddon 1/splatclient.sh
+++ Carmageddon 1/splatclient.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT_client.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_client.conf
-cd $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ && WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_client.conf -noconsole -c exit
+cd $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ && WINEPREFIX=$HOME/.local/share/Steam/steamapps/compatdata/282010/ WINEDLLOVERRIDES="3DfxSpl2,glide,glide2x,glide3x,winmm=n,b" wine $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosbox.exe -conf dosboxCARSPLAT.conf -conf dosboxCARSPLAT_client.conf -noconsole -c
