2ad05735b6ffa186448a7d080766f420a0a5b64a
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-21 22:39:32 -0300

    Added some configs for carma native

--- Carmageddon 1/carmanative.sh
+++ Carmageddon 1/carmanative.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+sed -i 's/output=\(.*\)/output=opengl/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
+sed -i "s/fullresolution=.*$/fullresolution=$(xrandr | grep '*' | awk '{print $1}')/" config_file $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
+sed -i 's/memsize=\(.*\)/memsize=512/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
+sed -i 's/glide=\(.*\)/glide=true/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
+sed -i 's/glide=\(.*\)/glide=true/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
