f67658da6ba0885d37332fd252ab782da80644b9
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-26 22:34:41 -0300

    compatibility issues fix

--- Carmageddon 1/Carmanative.sh
+++ Carmageddon 1/Carmanative.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-exec $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosbox-ece-x86_64.AppImage -conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf -conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf -noconsole -c
+LD_LIBRARY_PATH=/usr/local/lib dosbox-x -conf glide.conf
+cd ~/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ && ./dosbox-ece-x86_64.AppImage -conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf -conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf -noconsole -c
