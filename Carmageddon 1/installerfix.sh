afc2e2b23f1e69f12ea69fbb38da8058ca28b2cb
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-07-31 11:33:00 -0300

    removed unused directory

--- Carmageddon 1/installerfix.sh
+++ Carmageddon 1/installerfix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 links() {
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf $GAMEDIR/DOSBOX/dosboxCARMA.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf $GAMEIDR/DOSBOX/dosboxCARMA_single.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf $GAMEDIR/DOSBOX/dosboxCARSPLAT.conf
-ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT_single.conf $GAMEDIR/DOSBOX/dosboxCARSPLAT_single.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA_single.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARMA_single.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARSPLAT.conf
+ln -s $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARSPLAT_single.conf $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/dosboxCARSPLAT_single.conf
 }
 
 configs() {
