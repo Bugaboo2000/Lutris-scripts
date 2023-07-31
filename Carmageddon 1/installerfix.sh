bde474aa59f510d84d10884d37ec4b8fe78bc9ef
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-07-30 23:05:02 -0300

    cleaning unused code

--- Carmageddon 1/installerfix.sh
+++ Carmageddon 1/installerfix.sh
@@ -12,8 +12,6 @@ sed -i 's/output=\(.*\)/output=opengl/' $HOME/.local/share/Steam/steamapps/commo
 sed -i "s/fullresolution=.*$/fullresolution=$(xrandr | grep '*' | awk '{print $1}')/" config_file $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
 sed -i 's/memsize=\(.*\)/memsize=512/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
 sed -i 's/glide=\(.*\)/glide=true/' $HOME/.local/share/Steam/steamapps/common/Carmageddon1/dosboxCARMA.conf
-wget https://github.com/Bugaboo2000/Lutris-scripts/releases/download/Layer/nglide21.zip
-unzip nglide21.zip -d $HOME/.local/share/Steam/steamapps/common/Carmageddon1/DOSBOX/ 
 }
 
 splat() {
